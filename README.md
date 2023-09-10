f23be3b2c761d01e55278af4da7fce8a86f78d70
Rajat Chaurasia <er.rajat153@gmail.com>
2023-09-10 11:23:05 +0530

    chapter 3

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # React Tutuorial by Akshay Saini Sir 🚩🚩
+
+# create scripts to run.
+
+# React Element are objects when we render it to dom it becomes a DOM element.
+
+# 3 elements inside a React.creatElement are tag, attributes,childern.
+
+# JSX is not html in javascript, it is html like syntax.
+
+# Everything in react is component.
+
+# Functional Component, Class component
--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom/client"
 
+
+
+//React Element are objects when we render it to dom it becomes a DOM element
+
+
+
+//JSX
+const jsxheading = <h1>JSX heading</h1>
+
+//3 elements inside a creatElements are tag, attributes,childern
 // const heading = React.createElement('h1',{}, "Hello world from React")
-// const root = ReactDOM.createRoot(document.getElementById('root'))
+//  const root = ReactDOM.createRoot(document.getElementById('root'))
 // root.render(heading)
 
 
@@ -20,10 +30,10 @@ import ReactDOM from "react-dom"
  * 
  */
 // const first = React.createElement('div',{id:"child"});
-// const child = React.createElement('h1',{}, "Im h1 tags");
+// const child = React.createElement('h1',{}, "I am h1 tags");
 
 const parent  = React.createElement('div',{id:"parent"},
-React.createElement('div',{id:"child"}, [
+React.createElement('div', {id:"child"}, [
 React.createElement('h1',{}, "I m h1 tag"),
 React.createElement('h2',{}, "I m h2 tag")],
 
@@ -31,16 +41,38 @@ React.createElement("div", {id:"child2"},[
     React.createElement('h1',{},"I am Rajat"),
     React.createElement('h1',{},"Learning Namaste react course"),
 ])
+ ))
 
-))
+// // parent.render(child)
 
-// parent.render(child)
+// // console.log(parent) //object
 
-console.log(parent) //object
+// React Element
+const heading = (
+    <h1 className="head" tabIndex={4}>Namaste React Using JSX</h1>
+)
 
+console.log(typeof(heading)) // ans-object
+
+const Title = () =>{
+   return <h1>I am a Title</h1>
+}
+
+
+// React Functional Component return JSX
+//Component Composition
+const HeadingComponent = () => (
+    <div id ="container">
+        {Title()}
+        <Title/>
+        <Title></Title>   
+        <h2>Namsate Functional Component</h2>
+    </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(parent)
+// root.render(parent)
+root.render(<HeadingComponent/>)
 
 
